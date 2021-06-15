14ee738be6520493795101e6e704ce0cb5f7e48b
Alaa <alaa.mehrez@gmail.com>
2021-06-15 23:05:52 +0400

    version 1

---
+++ app.js
@@ -0,0 +1,12 @@
+(function() {
+
+  angular.module('myFirstApp', [])
+
+  .controller('LunchCheckController', function($scope){
+
+    $scope.name = "Yakoov";
+
+    var x1 = function () {
+  // do something, PLEASE!
+  return "Blah!";
+};
