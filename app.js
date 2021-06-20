214c11ed100862a76d5dad28a69ee0e90333f5ef
Alaa <alaa.mehrez@gmail.com>
2021-06-20 23:36:46 +0400

    version 4

--- app.js
+++ app.js
@@ -10,7 +10,37 @@
 
    function LunchCheckController($scope){
 
-    $scope.msg = "Check If Too Much";
+    //$scope.msg = "Check If Too Much";
+
+    $scope.myFunc = function() {
+
+      //console.log("value is:"+$scope.name);
+      if($scope.name==''|| $scope.name=='undefined')
+      $scope.msg="Please enter data first";
+      else {
+
+        const words = $scope.name.split(',');
+        //console.log("name"+ $scope.name);
+
+        var counter=0;
+
+          for(var i=0; i< words.length; i++)
+          counter++;
+
+          //console.log("counter"+counter);
+
+            if(counter<=3)
+                $scope.msg ="Enjoy!";
+            else {
+              $scope.msg ="Too much";
+            }
+
+      }
+
+
+
+    };
+
 
 }
 
